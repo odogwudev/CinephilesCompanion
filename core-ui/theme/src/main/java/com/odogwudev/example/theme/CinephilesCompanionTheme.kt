ecae857843ea8a8fbb5184529cb30bf17d933dd0
michaelenoma <enomamichael@gmail.com>
2023-01-25 00:05:53 +0100

    Project theme

---
+++ core-ui/theme/src/main/java/com/odogwudev/example/theme/CinephilesCompanionTheme.kt
@@ -0,0 +1,44 @@
+package com.odogwudev.example.theme
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import com.odogwudev.example.theme.attributes.*
+
+
+object AppTheme {
+    val dimens: AppDimens
+        @Composable @ReadOnlyComposable get() = LocalDimens.current
+
+    val colors: AppColor
+        @Composable @ReadOnlyComposable get() = LocalColors.current
+
+    val typography: AppTypography
+        @Composable @ReadOnlyComposable get() = LocalTypography.current
+
+    val shapes: AppShape
+        @Composable @ReadOnlyComposable get() = LocalShapes.current
+}
+
+@Composable
+fun CinephilesCompanionTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    colors: AppColor = if (isDarkTheme) darkColors else lightColors,
+    typography: AppTypography = if (isDarkTheme) darkTypography else lightTypography,
+    dimensions: AppDimens = AppTheme.dimens,
+    shapes: AppShape = AppTheme.shapes,
+    content: @Composable () -> Unit
+) {
+    val rippleIndication = rememberRipple()
+    CompositionLocalProvider(
+        LocalColors provides colors,
+        LocalDimens provides dimensions,
+        LocalTypography provides typography,
+        LocalShapes provides shapes,
+        LocalIndication provides rippleIndication,
+        content = content
+    )
+}
